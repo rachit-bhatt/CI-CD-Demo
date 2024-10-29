1841f32f6f076a07c0d4a6dc6735057d89fe3a06
Rachit <rachitbhatt2000@gmail.com>
2024-10-29 12:52:56 -0400

    Created Automative Docker Environment for the files.

---
+++ .dockerfile
@@ -0,0 +1,17 @@
+# Dockerfile
+
+# Use the official Python image from the Docker Hub
+FROM python:3.12-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file and install dependencies
+COPY requirements.txt requirements.txt
+RUN pip install --upgrade pip && pip install -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Command to run when the container starts
+CMD ["python", "main.py"]
---
+++ run_docker.ps1
@@ -0,0 +1,23 @@
+# run_docker.ps1
+
+# Define variables
+$imageName = "opencv_project"
+$containerName = "opencv_container"
+
+# Build the Docker image
+docker build -t $imageName .
+
+# Run the Docker container
+docker run --name $containerName $imageName
+
+# Check for errors
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "There was an error running the container."
+    exit 1
+}
+
+# Optionally, you can stop and remove the container afterward
+docker stop $containerName
+docker rm $containerName
+
+Write-Host "Execution completed successfully!"
